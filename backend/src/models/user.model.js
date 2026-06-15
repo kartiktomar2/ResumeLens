833501104af54b81f59f2f34ab51aa29ca75a40b
kartiktomar2 <kartiktomar1717@gmail.com>
2026-06-15 14:47:11 +0530

    added some packages and added a pre save hook for user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -28,5 +28,17 @@ const userSchema = new mongoose.Schema({
 
 
 }, { timestamps: true })
+
+
+userSchema.pre("save",async function(next){
+       if(!this.isModified("password"))
+       {
+        return  next()
+       }
+       this.password=await bcrypt.hash(this.password,10)
+       next()
+})
+
+//adding methods to check whether the user has entered the correct password or not 
 
 export  const User= mongoose.model("User", userSchema);
