74464d37171756fdfafa457fa0b58a5a2b3fc702
kartiktomar2 <kartiktomar1717@gmail.com>
2026-06-18 14:29:08 +0530

    added register user controller

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,4 +1,12 @@
 import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import jwt from "jsonwebtoken"
+
+
+
+
 
 /**
  * 
@@ -6,9 +14,71 @@ import { User } from "../models/user.model.js"
  * @param {*} res 
  * @description expects data and register a user 
  */
-const registerUser=async(req,res)=>{
-             
-}
 
 
-export {registerUser}
+const registerUser = asyncHandler(async (req, res) => {
+    //  register a user
+    // take username, email and password 
+    // check if user have entered it correctly
+    // check if a user with this email or username already exist or not 
+    // if yes then throw error and if not then create and save the user in db 
+
+    const { username, email, password } = req.body
+    if ([username, email, password].some(element => !element || element?.trim() === "")) {
+        throw new ApiError(400, 'provide email, username and password')
+    }
+
+    //validate email
+    if (!email.includes("@")) {
+        throw new ApiError(400, "Invalid email")
+    }
+
+
+    // finding existing user
+    const existingUser = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (existingUser) {
+        let errorMessage = (existingUser?.username === username) ? "A user with this username already exist" : (existingUser?.email === email) ? "A User with this email already exist" : "This user already exist "
+        throw new ApiError(400, errorMessage)
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password
+    })
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            username: user.username
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.TOKEN_EXPIRY
+        }
+    )
+
+    const options = {
+        httpOnly: true
+    }
+
+    res
+        .status(201)
+        .cookie("token", token, options)
+        .json(new ApiResponse(
+            "User created successfully",
+            201,
+            {
+                id: user._id,
+                username,
+                email
+            }
+        ))
+    //  res.cookie tell Express to send a Set-Cookie header in the HTTP response, now browser receives that response and store the cookies in the browser. 
+})
+
+
+export { registerUser }
