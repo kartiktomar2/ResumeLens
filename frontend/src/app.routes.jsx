3ed57439a4e93a2453fbeb98fcdb614c64fdc548
kartiktomar2 <kartiktomar1717@gmail.com>
2026-07-16 14:27:01 +0530

    created ui for interview report

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -3,6 +3,7 @@ import Login from "./features/auth/pages/Login.jsx"
 import Register from "./features/auth/pages/Register.jsx"
 import ProtectedRoutes from "./features/auth/components/ProtectedRoutes.jsx"
 import Home from "./features/interview/pages/Home.jsx"
+import Interview from "./features/interview/pages/Interview.jsx"
 
 export const router= createBrowserRouter([
        {
@@ -16,6 +17,10 @@ export const router= createBrowserRouter([
       {
           path:"/register",
           element:<Register/>
+      },
+      {
+          path:"/interview/:interviewId",
+           element: <ProtectedRoutes><Interview/> </ProtectedRoutes>
       }
 
 ])
